6dbf337743c4c872677a5c55ffd5462bc0334f49
Trung Hieu <hieu@ltv.dev>
2023-03-29 10:26:23 +0700

    Adding re-usable Button.js

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from '../UI/Button';
 import Card from '../UI/Card';
 
 import classes from './AddUser.module.css';
@@ -15,7 +16,7 @@ function AddUser() {
                 <input type="text" id="username" />
                 <label htmlFor="age">Age:</label>
                 <input type="number" id="age" />
-                <button type="submit">Add User</button>
+                <Button type="submit">Add User</Button>
             </form>
         </Card>
     );
